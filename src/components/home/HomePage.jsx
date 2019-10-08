5c9f74c5f1f64b04ca9db33cb0282f16f16590af
shamim reza <reza_cse08@hotmail.com>
2019-10-09 00:02:42 +0600

    Before using Redux

---
+++ src/components/home/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// eslint-disable-next-line no-extra-parens
+const HomePage = () => (
+  <div className="jumbotron">
+    <h1>Pluralsight Administration</h1>
+    <p>React, Redux and React Router for ultra-responsive web apps.</p>
+    <Link to="about" className="btn btn-primary btn-lg">
+      Learn more
+    </Link>
+  </div>
+);
+
+export default HomePage;
